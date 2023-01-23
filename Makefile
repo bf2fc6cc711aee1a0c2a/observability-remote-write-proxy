7ecade5ba863cd4777ee48d07bf31f7aa0031372
Peter Braun <pbraun@redhat.com>
2023-01-23 17:17:18 +0100

    wip: proxy remote write requests using httputil reverse proxy

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
-prom:
-	docker run -p 9090:9090 --network=host -v $(shell pwd)/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus --web.enable-remote-write-receiver --config.file=/etc/prometheus/prometheus.yml
+prom1:
+	docker run --network=host -v $(shell pwd)/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus --web.enable-remote-write-receiver --config.file=/etc/prometheus/prometheus.yml
+
+prom2:
+	docker run --network=host -v $(shell pwd)/prometheus2.yml:/etc/prometheus/prometheus.yml prom/prometheus --web.listen-address=:9091 --web.enable-remote-write-receiver --config.file=/etc/prometheus/prometheus.yml
--- main.go
+++ main.go
@@ -3,16 +3,75 @@ package main
 import (
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"observability-remote-write-proxy/pkg/remotewrite"
+	"strings"
+)
+
+const (
+	prefixHeader = "X-Forwarded-Prefix"
 )
 
 func main() {
-	handler := http.HandlerFunc(handleRequestSuccess)
-	http.Handle("/", handler)
-	err := http.ListenAndServe(":8080", nil)
+	upstreamUrl, err := url.Parse("http://localhost:9091/api/v1/write")
+	if err != nil {
+		panic(err)
+	}
+
+	proxy := httputil.ReverseProxy{
+		Director: func(request *http.Request) {
+			request.URL.Scheme = upstreamUrl.Scheme
+			// Set the Host at both request and request.URL objects.
+			request.Host = upstreamUrl.Host
+			request.URL.Host = upstreamUrl.Host
+			// Derive path from the paths of configured URL and request URL.
+			request.URL.Path = upstreamUrl.Path
+			// Add prefix header with value "/", since from a client's perspective
+			// we are forwarding /<anything> to /<cfg.upstream.url.Path>/<anything>.
+			request.Header.Add(prefixHeader, "/")
+		},
+		ErrorHandler: func(writer http.ResponseWriter, request *http.Request, err error) {
+			log.Println(err)
+		},
+	}
+
+	proxy.Transport = http.DefaultTransport
+
+	http.Handle("/", &proxy)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		return
 	}
+}
+
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
+}
+
+func joinURLPath(a, b *url.URL) (path, rawpath string) {
+	if a.RawPath == "" && b.RawPath == "" {
+		return singleJoiningSlash(a.Path, b.Path), ""
+	}
+	apath := a.EscapedPath()
+	bpath := b.EscapedPath()
+	aslash := strings.HasSuffix(apath, "/")
+	bslash := strings.HasPrefix(bpath, "/")
+	switch {
+	case aslash && bslash:
+		return a.Path + b.Path[1:], apath + bpath[1:]
+	case !aslash && !bslash:
+		return a.Path + "/" + b.Path, apath + "/" + bpath
+	}
+	return a.Path + b.Path, apath + bpath
 }
 
 func handleRequestSuccess(w http.ResponseWriter, r *http.Request) {
