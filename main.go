d676529ada47760ce6f8479f9c6ad3065a19a6c0
valeriiashapoval <shapoval.valeriia@outlook.com>
2023-01-27 14:18:52 +0000

    address comments

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func main() {
 
 	proxy.Transport = http.DefaultTransport
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		err := validateRequest(w, r)
+		err := validateRequest(r)
 		if err != nil {
 			w.WriteHeader(http.StatusForbidden)
 		} else {
@@ -50,28 +50,26 @@ func main() {
 	}
 }
 
-func validateRequest(w http.ResponseWriter, r *http.Request) error {
+func validateRequest(r *http.Request) error {
 	log.Println("request received")
 
 	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusNotFound)
 		return errors.New("not found")
 	}
 
 	writeRequest, err := remotewrite.DecodeWriteRequest(r.Body)
 	if err != nil {
 		log.Printf("error receiving remote write request: %v", err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
 		return errors.New("error receiving remote write request")
 	}
+
+	log.Printf("there are %v time series in the request", len(writeRequest.Timeseries))
 
 	clusterIDs := remotewrite.FindClusterIDs(writeRequest)
 	if len(clusterIDs) > 1 {
 		log.Printf("request contains multiple cluster IDs: %v", clusterIDs)
-		w.WriteHeader(http.StatusBadRequest)
 		return errors.New("request contains multiple cluster IDs")
 	}
 
-	log.Printf("there were %v time series in the request", len(writeRequest.Timeseries))
 	return nil
 }
